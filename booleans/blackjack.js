6ea69046122021af76a7e92f93b34ba1b29320df
casgeosn-cmd <cas.geosn@gmail.com>
2026-09-04 22:04:32 -0400

    new file:   booleans/blackjack.js

---
+++ booleans/blackjack.js
@@ -0,0 +1,28 @@
+// player cards
+let cardOne = 7;
+let cardTwo = 5;
+let sum = cardOne + cardTwo; // 15
+// bank cards
+let cardOneBank = 7;
+let cardTwoBank = 5;
+let cardThreeBank = 6;
+let cardFourBank = 4;
+
+let cardThree = 7;
+sum += cardThree;
+
+if (sum> 21) {
+    console.log('You lost');
+    process.exit(1); // exit program
+}
+console.log('you have ${sum} points');
+
+let bankSum = cardOneBank + cardTwoBank + cardThreeBank + cardFourBank;
+
+if (bankSum > 21 || (sum <= 21 && sum > bankSum)) {
+  console.log('You win');
+  process.exit(1); // exit program
+} else {
+  console.log('Bank wins');
+}
+// opponent: the bank
